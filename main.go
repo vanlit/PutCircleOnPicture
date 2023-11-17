6323eb943ba4f85c7f9896cd815918233fbda7e2
vanlit <i2.ilin@itransition.com>
2023-11-17 20:18:15 +0100

    implement PutSquareOnPicture as a CLI program putting a single square on a picture, thanks chatgpt for ~85% of the job

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 7 {
+		fmt.Println("Usage: go run main.go <input_image_path> <output_image_path> <x> <y> <color> <size>")
+		os.Exit(1)
+	}
+
+	inputImagePath := os.Args[1]
+	outputImagePath := os.Args[2]
+	x, y, size, err := validateInputs(os.Args[3], os.Args[4], os.Args[6])
+	if err != nil {
+		fmt.Printf("Error validating inputs: %v\n", err)
+		os.Exit(1)
+	}
+
+	colorRGB := parseColor(os.Args[5])
+
+	err = processImage(inputImagePath, outputImagePath, x, y, colorRGB, size)
+	if err != nil {
+		fmt.Printf("Error processing image: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Image with colored square saved to %s\n", outputImagePath)
+}
+
+
+func processImage(inputPath, outputPath string, x, y int, clr color.RGBA, size int) error {
+	// Read the input image
+	img, err := readImage(inputPath)
+	if err != nil {
+		return fmt.Errorf("error reading input image: %v", err)
+	}
+
+	// Create a new draw.Image from the existing image
+	drawImg := image.NewRGBA(img.Bounds())
+	draw.Draw(drawImg, drawImg.Bounds(), img, image.Point{}, draw.Over)
+
+	// Draw the colored square on the image
+	drawSquare(drawImg, x, y, clr, size)
+
+	// Save the modified image to the output path
+	err = saveImage(outputPath, drawImg)
+	if err != nil {
+		return fmt.Errorf("error saving output image: %v", err)
+	}
+
+	return nil
+}
+
+func validateInputs(xStr, yStr, sizeStr string) (x, y, size int, err error) {
+	// Validate x and y coordinates
+	x, err = validatePositiveInt(xStr)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid x coordinate: %v", err)
+	}
+
+	y, err = validatePositiveInt(yStr)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid y coordinate: %v", err)
+	}
+
+	// Validate square size
+	size, err = validatePositiveInt(sizeStr)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid square size: %v", err)
+	}
+
+	return x, y, size, nil
+}
+
+func validatePositiveInt(str string) (int, error) {
+	value := parseInt(str)
+
+	if value <= 0 {
+		return 0, fmt.Errorf("value must be a positive number")
+	}
+
+	return value, nil
+}
+
+func parseCoordinates(xStr, yStr string) (x, y int) {
+	fmt.Sscanf(xStr, "%d", &x)
+	fmt.Sscanf(yStr, "%d", &y)
+	return
+}
+
+func parseColor(colorStr string) color.RGBA {
+	var r, g, b uint8
+	fmt.Sscanf(colorStr, "%02x%02x%02x", &r, &g, &b)
+	return color.RGBA{r, g, b, 255}
+}
+
+func parseInt(sizeStr string) int {
+	var size int
+	fmt.Sscanf(sizeStr, "%d", &size)
+	return size
+}
+
+func readImage(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	
+
+	return img, nil
+}
+
+func drawSquare(img draw.Image, x, y int, clr color.RGBA, size int) {
+	draw.Draw(img, image.Rect(x, y, x+size, y+size), &image.Uniform{clr}, image.Point{}, draw.Over)
+}
+
+func saveImage(path string, img image.Image) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = jpeg.Encode(file, img, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+dateForName=`date +%Y-%M-%d_%H-%m-%S`
+go run main.go testInputs/input.jpg "testOutputs/output_${dateForName}.jpg" 200 100 ffffff 25
